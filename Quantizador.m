eb25b6c49379d5d8fd123b6769b31e10f3e3d430
Arthur Medeiros Guimarães <arthurmgwork@users.noreply.github.com>
2021-02-02 10:50:30 -0300

    Update Quantizador.m

--- Quantizador.m
+++ Quantizador.m
@@ -11,10 +11,10 @@ clc
 double t; %torna o t(tempo) como uma variavel double para ter mais casas decimais
 t=[0]; %iniciar o t(tempo) como um(a) vetor/matriz (1,1) para servir de inicio ao vetor do tempo do audio
 
-while length(t) < length(x) %enquanto o numero de pontos do tempo for menor que o numero de pontos do sinal de entrada ele fará a condição abaixo  
+while length(t) < length(x) %enquanto o numero de pontos do tempo for menor que o numero de pontos do sinal de entrada ele farï¿½ a condiï¿½ï¿½o abaixo  
 	
-    t(length(t)+1)=t(length(t))+(1/Fs); %irá aumentar o valor do vetor tempo no passo da frequencia de amostragem (convertido para segundos (1/Fs)
-    %o valor de tempo adicionado será sempre na linha abaixo da ultima linha do vetor do tempo
+    t(length(t)+1)=t(length(t))+(1/Fs); %irï¿½ aumentar o valor do vetor tempo no passo da frequencia de amostragem (convertido para segundos (1/Fs)
+    %o valor de tempo adicionado serï¿½ sempre na linha abaixo da ultima linha do vetor do tempo
 end
 
 Y = fft(x); %Encontra a frequencia rapida de fourier (fast frequency fourier - fft)
@@ -51,72 +51,72 @@ grid on
 
 
 
-%% Caracteristicas da Quantização
+%% Caracteristicas da Quantizaï¿½ï¿½o
 
 PP = max(x)-min(x); %pico a pico do sinal
 b = 8; %bits do sinal a ser quantizado
-L = 2^b; %Quantidade de niveís de quantização
-v = PP/L; %largura dos niveis da quantização
-n = min(x):v:max(x); %Niveis de quantização
-n = [n(1,1:length(n)-1) n(1,length(n)-1)]; %Modifica o ultimo valor do nivel de quantização para ele ficar interpolado com o tempo de quantização
-q_t = min(x)-(v/2):v:max(x)+(v/2); %Tempo de quantização, espaçamento horizontal da quantização com um off_set para ajustar o posicionamento na posição (0,0)
+L = 2^b; %Quantidade de niveï¿½s de quantizaï¿½ï¿½o
+v = PP/L; %largura dos niveis da quantizaï¿½ï¿½o
+n = min(x):v:max(x); %Niveis de quantizaï¿½ï¿½o
+n = [n(1,1:length(n)-1) n(1,length(n)-1)]; %Modifica o ultimo valor do nivel de quantizaï¿½ï¿½o para ele ficar interpolado com o tempo de quantizaï¿½ï¿½o
+q_t = min(x)-(v/2):v:max(x)+(v/2); %Tempo de quantizaï¿½ï¿½o, espaï¿½amento horizontal da quantizaï¿½ï¿½o com um off_set para ajustar o posicionamento na posiï¿½ï¿½o (0,0)
 q_t = [min(q_t)+(v/2) q_t(2:length(q_t)-1) max(q_t)-(v/2)]; %Eliminando o off_set aplicado na linha acima desta
 
 
-%% Digitalização do sinal
+%% Digitalizaï¿½ï¿½o do sinal
 
-digita=zeros(1,length(x)); %Criação do sinal que será substituido pelos valores a serem digitalizados
+digita=zeros(1,length(x)); %Criaï¿½ï¿½o do sinal que serï¿½ substituido pelos valores a serem digitalizados
 
-for i=1:length(x) %% Varia de 1 até a quantidade de pontos da amostra do sinal de entrada
-    for j=1:length(q_t)-1  %% Varia de um até a quantidade de pontos do tempo de quantização menos o ultimo ponto (-1) (o -1 é para não dar erro de matriz no if abaixo, no momento em que J for igual ao ultimo ponto do tempo de quantização q_t)
-        if x(i)>=q_t(j) && x(i)<=q_t(j+1); %% Verifica se o sinal esta em determinada faixa de do tempo de quantização, entre o atual (j) e o proximo (j+1)
-        	digita(i)=n(j); %% Declara a digitalização "i", em relação ao tempo de quantização "j" com o nivel da quantização "j"
+for i=1:length(x) %% Varia de 1 atï¿½ a quantidade de pontos da amostra do sinal de entrada
+    for j=1:length(q_t)-1  %% Varia de um atï¿½ a quantidade de pontos do tempo de quantizaï¿½ï¿½o menos o ultimo ponto (-1) (o -1 ï¿½ para nï¿½o dar erro de matriz no if abaixo, no momento em que J for igual ao ultimo ponto do tempo de quantizaï¿½ï¿½o q_t)
+        if x(i)>=q_t(j) && x(i)<=q_t(j+1); %% Verifica se o sinal esta em determinada faixa de do tempo de quantizaï¿½ï¿½o, entre o atual (j) e o proximo (j+1)
+        	digita(i)=n(j); %% Declara a digitalizaï¿½ï¿½o "i", em relaï¿½ï¿½o ao tempo de quantizaï¿½ï¿½o "j" com o nivel da quantizaï¿½ï¿½o "j"
         end
     end
 end
 
 
-%% Plot do Sinal de Entrada e do Sinal Digitalizado não Amostrado
+%% Plot do Sinal de Entrada e do Sinal Digitalizado nï¿½o Amostrado
 
 figure()
 hold on
 plot(t,x);
 plot(t,digita);
-title(['Sinal Não Amostrado Quantizado a uma Taxa de ', num2str(b), ' Bits'])
+title(['Sinal Nï¿½o Amostrado Quantizado a uma Taxa de ', num2str(b), ' Bits'])
 legend('Sinal de Entrada','Sinal Digitalizado')
 xlabel('Tempo')
 ylabel('Amplitude')
 axis([min(t) max(t) min(x)-1 max(x)+1])
 grid on
 
-%% Teorema de Nyquist–Shannon
+%% Teorema de Nyquistï¿½Shannon
 
 fmax = max(f); %FREQUENCIA MAXIMA - encontra a maxima frequencia determinada no espectro da frequencia
 f_sample = 4*fmax; %FREQUENCIA DE AMOSTRAGEM - torna a frequencia da amostra 4 vezes maior que a frequencia maxima (o minimo tem de ser 2)
-amostragem = f_sample/Fs; %determina quantas veses a frequencia de amostragem é maior que a frequencia de amostras do sinal analisado, isso vai servir como passo para amostrar o sinal
+amostragem = f_sample/Fs; %determina quantas veses a frequencia de amostragem ï¿½ maior que a frequencia de amostras do sinal analisado, isso vai servir como passo para amostrar o sinal
 t_sample = t(1,1:amostragem:(length(t))); % TEMPO AMOSTRADO - seleciona os pontos do tempo no passo da amostragem
 x_sample = x(1:amostragem:(length(x)),1); % SINAL DE ENTRADA AMOSTRADO - seleciona os pontos do sinal de entrada no passo da amostragem
 
-%% Digitalização do Sinal de Entrada amostrado
+%% Digitalizaï¿½ï¿½o do Sinal de Entrada amostrado
 
-digita_sample=zeros(1,length(x_sample)); %Criação do sinal que será substituido pelos valores a serem digitalizados do sinal de entrada amostrado
+digita_sample=zeros(1,length(x_sample)); %Criaï¿½ï¿½o do sinal que serï¿½ substituido pelos valores a serem digitalizados do sinal de entrada amostrado
 
-for i=1:length(x_sample) %% Varia de 1 até a quantidade de pontos da amostra do sinal de entrada amostrado
-    for j=1:length(q_t)-1  %% Varia de um até a quantidade de pontos do tempo de quantização menos o ultimo ponto (-1) (o -1 é para não dar erro de matriz no if abaixo, no momento em que J for igual ao ultimo ponto do tempo de quantização q_t)
-        if x_sample(i)>=q_t(j) && x_sample(i)<=q_t(j+1); %% Verifica se o sinal esta em determinada faixa de do tempo de quantização, entre o atual (j) e o proximo (j+1)
-        	digita_sample(i)=n(j); %% Declara a digitalização amostrada "i", em relação ao tempo de quantização "j" com o nivel da quantização "j"
+for i=1:length(x_sample) %% Varia de 1 atï¿½ a quantidade de pontos da amostra do sinal de entrada amostrado
+    for j=1:length(q_t)-1  %% Varia de um atï¿½ a quantidade de pontos do tempo de quantizaï¿½ï¿½o menos o ultimo ponto (-1) (o -1 ï¿½ para nï¿½o dar erro de matriz no if abaixo, no momento em que J for igual ao ultimo ponto do tempo de quantizaï¿½ï¿½o q_t)
+        if x_sample(i)>=q_t(j) && x_sample(i)<=q_t(j+1); %% Verifica se o sinal esta em determinada faixa de do tempo de quantizaï¿½ï¿½o, entre o atual (j) e o proximo (j+1)
+        	digita_sample(i)=n(j); %% Declara a digitalizaï¿½ï¿½o amostrada "i", em relaï¿½ï¿½o ao tempo de quantizaï¿½ï¿½o "j" com o nivel da quantizaï¿½ï¿½o "j"
         end
     end
 end
 
 
-%% Plot do Sinal Digital Utilizando o teorema de Nyquist–Shannon
+%% Plot do Sinal Digital Utilizando o teorema de Nyquistï¿½Shannon
 
 figure()
 hold on
 plot(t,x)
 plot(t_sample,digita_sample)
-title(['Teorema de Nyquist–Shannon - Sinal Quantizado a uma Taxa de ', num2str(b), ' Bits'])
+title(['Teorema de Nyquistï¿½Shannon - Sinal Quantizado a uma Taxa de ', num2str(b), ' Bits'])
 legend('Sinal de Entrada','Sinal Digitalizado Amostrado')
 xlabel('Tempo')
 ylabel('Amplitude')
@@ -125,50 +125,6 @@ grid on
 
 
 
-
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% DESCONSIDERAR DAQUI PARA BAIXO %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
-
-%% Tentativa de Codificação
-
-% wordlength = log2(L); %saber quantos bits serão necessarios para a codificação
-% for i=1:length(sinal_digit)
-%     teste(i,1) = fi(sinal_digit(i),1,wordlength);
-% end
-% testando = teste.bin;
-% 
-
-
-%% Plot do Erro de Quantização (Entrada - Saída)
-% Vrms=x/sqrt(2); %Tensão RMS
-% P=Vrms.^2; %Potencia do Sinal
-% P_R=(1/12)*((PP/L).^2); %Potência do Ruído de Quantização (Erro Médio Quadrático de Quantização)
-% R_Q=P/P_R; %Sinal Para Ruído de Quantização
-% 
-% 
-% figure()
-% subplot(2,1,1);
-% plot(t,x)
-% hold on
-% plot(t,digita);
-% title(['Sinal Quantizado a uma Taxa de ', num2str(b), ' Bits'])
-% legend('Sinal de Entrada','Sinal Quantizado')
-% xlabel('Tempo')
-% ylabel('Tensão')
-% % axis([min(t) max(t) min(x)-1 max(x)+1])
-% grid on
-% 
-% subplot(2,1,2);
-% plot(t_sample,sinal_digit,'.k');
-% title('Erro de Quantização')
-% legend('Erro de quantização')
-% xlabel('Tempo')
-% ylabel('Tensão')
-% % axis([min(t) max(t) min(x)-1 max(x)+1])
-% grid on
 
 
 
